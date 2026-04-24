51129c5756767a06484563c48b6dfaa6d8104538
Vladimir Zarypov <krre31@gmail.com>
2026-04-24 07:51:02 +0300

    Refactor token expired time

--- src/normhub/core/jwt.rs
+++ src/normhub/core/jwt.rs
@@ -3,7 +3,7 @@ use std::{
     time::{Duration, SystemTime},
 };
 
-use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation, decode, encode};
 use serde::{Deserialize, Serialize};
 use sqlx::types::time::OffsetDateTime;
 use thiserror::Error;
@@ -20,7 +20,7 @@ struct Claims {
 pub struct Error(#[from] jsonwebtoken::errors::Error);
 
 pub fn create_token(user_id: i64, email: &str, secret: &str) -> Result<String, Error> {
-    let from_now = Duration::from_secs(3600 * 24 * 365 * 10); // 10 years
+    let from_now = Duration::from_hours(24 * 365 * 10); // 10 years
     let expired_future_time = SystemTime::now().add(from_now);
     let exp = OffsetDateTime::from(expired_future_time);
 
